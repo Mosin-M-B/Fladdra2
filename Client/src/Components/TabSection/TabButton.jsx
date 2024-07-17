dacc0ede93eb495c79554c688903cc46d6d19e66
Mosin-M-B <mosinbalsing@gmail.com>
2024-07-17 18:16:24 +0530

    list type

--- Client/src/Components/TabSection/TabButton.jsx
+++ Client/src/Components/TabSection/TabButton.jsx
@@ -5,7 +5,7 @@ import React from "react";
 export default function TabButton({ children, onSelect, isSelected }) {
   console.log('TABBUTTON COMPONENT EXECUTING');
   return (
-    <li>
+    <li style={{listStyleType:'none'}}>
       <button style = {{width:'100%',height:'150px', textAlign:'left' , marginRight:'20px'}}className={isSelected ? 'activee' : undefined} onClick={onSelect}>
         {children}
       </button>
