b30500b0d71738c1cab450a475d82cf6dd6832db
Mosin-M-B <mosinbalsing@gmail.com>
2024-07-17 18:25:27 +0530

    tabsection css

--- Client/src/Components/TabSection/TabButton.jsx
+++ Client/src/Components/TabSection/TabButton.jsx
@@ -6,7 +6,7 @@ export default function TabButton({ children, onSelect, isSelected }) {
   console.log('TABBUTTON COMPONENT EXECUTING');
   return (
     <li style={{listStyleType:'none' ,backgroundColor:'none' }}>
-      <button style = {{width:'100%',height:'150px', textAlign:'left' , marginRight:'20px'}}className={isSelected ? 'activee' : undefined} onClick={onSelect}>
+      <button id='tabbutton' className={isSelected ? 'activee' : undefined} onClick={onSelect}>
         {children}
       </button>
     </li>
