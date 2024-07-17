33c6fc0a3977266deba86018342c4b00fb6e3878
Mosin-M-B <mosinbalsing@gmail.com>
2024-07-17 18:52:30 +0530

    remove Button

--- Client/src/Components/TabSection/TabButton.jsx
+++ Client/src/Components/TabSection/TabButton.jsx
@@ -5,10 +5,8 @@ import React from "react";
 export default function TabButton({ children, onSelect, isSelected }) {
   console.log('TABBUTTON COMPONENT EXECUTING');
   return (
-    <li style={{listStyleType:'none' ,backgroundColor:'none' }}>
-      <button id='tabbutton' className={isSelected ? 'activee' : undefined} onClick={onSelect}>
+    <li style={{listStyleType:'none' ,backgroundColor:'none' , cursor:'pointer'}} className={isSelected ? 'activee' : undefined} onClick={onSelect}>
         {children}
-      </button>
     </li>
   );
 }
