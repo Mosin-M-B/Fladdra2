71f838febc6db021fac5d8e67d676baa0a53d2bb
Mosin-M-B <mosinbalsing@gmail.com>
2024-07-17 19:04:05 +0530

    adding class dynamicly

--- Client/src/Components/TabSection/TabButton.jsx
+++ Client/src/Components/TabSection/TabButton.jsx
@@ -5,7 +5,7 @@ import React from "react";
 export default function TabButton({ children, onSelect, isSelected }) {
   console.log('TABBUTTON COMPONENT EXECUTING');
   return (
-    <li id="tabbutton"style={{listStyleType:'none' ,backgroundColor:'none' , cursor:'pointer'}} className={isSelected ? 'activee' : undefined} onClick={onSelect}>
+    <li  style={{listStyleType:'none' ,backgroundColor:'none' , cursor:'pointer'}} className={isSelected ? 'activee' : 'tabbutton'} onClick={onSelect}>
         {children}
     </li>
   );
